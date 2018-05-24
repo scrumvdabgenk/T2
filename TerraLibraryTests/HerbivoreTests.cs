f249d0d1dbdaee4f41740e173b3a08721b639b16
davyraes <davy-raes@telenet.be>
2018-05-24 13:44:22 +0200

    d

--- TerraLibraryTests/HerbivoreTests.cs
+++ TerraLibraryTests/HerbivoreTests.cs
@@ -7,22 +7,35 @@ namespace TerraLibraryTests
     [TestClass]
     public class HerbivoreTests
     {
+        private Terrarium BeginTerrarium;
+        private Terrarium EindTerrarium;
+        private Herbivore herbivore;
+        private Plant plant;
         [TestInitialize]
         public void Initialize()
         {
-            Terrarium terrarium = new Terrarium(1, 2);
-            terrarium
+            BeginTerrarium = new Terrarium(1, 2);
+            EindTerrarium = BeginTerrarium;
+            herbivore = new Herbivore();
+            plant = new Plant();
         }
         [TestMethod]
         public void HerbivoreConstructorTest()
-        {
-            Herbivore herbivore = new Herbivore();
+        {            
             Assert.AreEqual("H", herbivore.DisplayLetter);
         }
         [TestMethod]
-        public void HerbivoreEatsPlantOnRightSide()
+        public void PlantOnRightSideOfHerbivore()
         {
+            BeginTerrarium.TerrariumItems[0, 0] = herbivore;
+            BeginTerrarium.TerrariumItems[0, 1] = plant;
 
+            Assert.AreEqual(true, herbivore.CheckPlantRight());
+        }
+        [TestMethod]
+        public void HerbivoorMoveTest()
+        {
+            BeginTerrarium.TerrariumItems[0, 0] = herbivore;
         }
     }
 }
