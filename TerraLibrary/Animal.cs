aa6b936778a0a2a3e610dd9d4e60108d2a84027b
davyraes <davy-raes@telenet.be>
2018-05-24 15:59:14 +0200

    einde dag 24/05

--- TerraLibrary/Animal.cs
+++ TerraLibrary/Animal.cs
@@ -8,6 +8,7 @@ namespace TerraLibrary
 {
     public abstract class Animal: Organism
     {
+        private Random random = new Random();
         /* Constructor */
         public Animal(Position position, Terrarium terrarium)
             :base(position, terrarium)
@@ -15,19 +16,71 @@ namespace TerraLibrary
         }
 
         /* Methods */
-        private void Move()
+        public void Move()
         {
-            throw new NotImplementedException();
+            Move(random.Next(1, 4));
         }
-        private void Move(int direction)
+        public void Move(int direction)
         {
-            throw new NotImplementedException();
+            if(!CheckRight())
+            {
+                switch (direction)
+                {
+                    case 1:
+                        Position.Y -= 1;
+                        break;
+                    case 2:
+                        Position.X += 1;
+                        break;
+                    case 3:
+                        Position.Y += 1;
+                        break;
+                    case 4:
+                        Position.X -= 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
         private bool CheckRight()
         {
             foreach(Organism organism in Terrarium.Organisms )
             {
                 if(organism.Position==new Position(Position.X + 1,Position.Y))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool CheckBelow()
+        {
+            foreach (Organism organism in Terrarium.Organisms)
+            {
+                if (organism.Position == new Position(Position.X, Position.Y-1))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool CheckLeft()
+        {
+            foreach (Organism organism in Terrarium.Organisms)
+            {
+                if (organism.Position == new Position(Position.X - 1, Position.Y))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool CheckAbove()
+        {
+            foreach (Organism organism in Terrarium.Organisms)
+            {
+                if (organism.Position == new Position(Position.X, Position.Y-1))
                 {
                     return true;
                 }
