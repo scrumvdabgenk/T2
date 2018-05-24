1ec6d3ae8e72b603d98a0d71f818f10d441a8c66
davyraes <davy-raes@telenet.be>
2018-05-24 10:50:13 +0200

    Nieuwe classlibrary gemaakt

---
+++ UnitTestProject1/PlantTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TerraLibrary;
+
+namespace TerraTeam2Tests
+{
+    [TestClass]
+    public class PlantTests
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
