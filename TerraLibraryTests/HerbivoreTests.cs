34eb6b4146e7cf02723b9fc503096e62b9ee6c6a
davyraes <davy-raes@telenet.be>
2018-05-24 11:50:03 +0200

    gr

--- TerraLibraryTests/HerbivoreTests.cs
+++ TerraLibraryTests/HerbivoreTests.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TerraLibrary;
 
 namespace TerraLibraryTests
 {
     [TestClass]
     public class HerbivoreTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestInitialize]
+        public void Initialize()
         {
+            Terrarium terrarium = new Terrarium(1, 2);
+            terrarium
+        }
+        [TestMethod]
+        public void HerbivoreConstructorTest()
+        {
+            Herbivore herbivore = new Herbivore();
+            Assert.AreEqual("H", herbivore.DisplayLetter);
+        }
+        [TestMethod]
+        public void HerbivoreEatsPlantOnRightSide()
+        {
+
         }
     }
 }
