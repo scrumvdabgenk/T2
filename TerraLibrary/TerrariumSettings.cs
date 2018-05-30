7c7ca950a7ced8df5ca5abbc79e8863f4f7824fe
dansmts <dan.smets@gmail.com>
2018-05-30 10:42:47 +0200

    Changes to screencontroller

--- TerraLibrary/TerrariumSettings.cs
+++ TerraLibrary/TerrariumSettings.cs
@@ -8,12 +8,17 @@ namespace TerraLibrary
 {
     public class TerrariumSettings
     {
+        private int height;
+
         public int Height { get; set; }
         public int Width { get; set; }
         public int Plants { get; set; }
         public int Herbivores { get; set; }
         public int Carnivores { get; set; }
         public int Humans { get; set; }
+        public Frame DictionaryFrame { get; set; }
+
+        private static Dictionary<string, string> PropErrors { get; set; }
 
         public TerrariumSettings()
         {
@@ -23,6 +28,16 @@ namespace TerraLibrary
             Herbivores = 10;
             Carnivores = 10;
             Humans = 10;
+
+            PropErrors = new Dictionary<string, string>()
+            {
+                { "Height" , "Height must be between 15 and " + (Console.LargestWindowHeight - 5) },
+                { "Width" , "Width must be between 15 and " + (Console.LargestWindowWidth -1) },
+                { "Plants" , "Plants can be max " + (this.Height* this.Width / 4) },
+                { "Herbivores" , "Herbivores can be max " + (this.Height* this.Width / 4) },
+                { "Carnivores" , "Carnivores can be max " + (this.Height* this.Width / 4) },
+                { "Humans" , "Humans can be max " + (this.Height* this.Width / 4) }
+            };
         }
 
         public void ResetSettings()
@@ -53,13 +68,44 @@ namespace TerraLibrary
         private int ReadNumber(string prop, int cursorPosition)
         {
             int number;
+            bool isNotAcceptableNumber = true;
+            
+
             do
             {
+                
                 Console.SetCursorPosition(14, cursorPosition);
                 Console.Write(prop + ": ");
-                number = int.Parse(Console.ReadLine());
-            } while (number <= 0);
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    isNotAcceptableNumber = false;
+                }
+                else
+                {
+                    Console.SetCursorPosition(14, cursorPosition + 1);
+                    Console.Write(PropErrors[prop]);
+                }
+            } while (isNotAcceptableNumber);
+
+            string clearLine = "                                           ";
+            Console.SetCursorPosition(14, cursorPosition + 1);
+            Console.Write(clearLine);
+
             return number;
+        }
+    }
+
+    public class Frame
+    {
+        public string Name { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public Frame(string name, int min, int max)
+        {
+            Name = name;
+            Min = min;
+            Max = max;
         }
     }
 }
