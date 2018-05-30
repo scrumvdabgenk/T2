0a40b89586e55724b2b66da04bebb5e900c339d6
dansmts <dan.smets@gmail.com>
2018-05-30 12:26:49 +0200

    Changed ScreenController

--- TerraLibrary/Menu.cs
+++ TerraLibrary/Menu.cs
@@ -77,5 +77,76 @@ namespace TerraLibrary
 
             return currentSelection;
         }
+
+        public static int ShowFiles(int x, int y, bool canCancel, List<string> options)
+        {
+            int startX = x;
+            int startY = y;
+            const int optionsPerLine = 1;
+            const int spacingPerLine = 30;
+
+            int currentSelection = 0;
+
+            ConsoleKey key;
+
+            Console.CursorVisible = false;
+
+            do
+            {
+                for (int i = 0; i < options.Count; i++)
+                {
+                    Console.SetCursorPosition(startX + (i % optionsPerLine) * spacingPerLine, startY + i / optionsPerLine);
+
+                    if (i == currentSelection)
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+                    Console.Write(options[i]);
+
+                    Console.ResetColor();
+                }
+
+                key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        {
+                            if (currentSelection % optionsPerLine > 0)
+                                currentSelection--;
+                            break;
+                        }
+                    case ConsoleKey.RightArrow:
+                        {
+                            if (currentSelection % optionsPerLine < optionsPerLine - 1)
+                                currentSelection++;
+                            break;
+                        }
+                    case ConsoleKey.UpArrow:
+                        {
+                            if (currentSelection >= optionsPerLine)
+                                currentSelection -= optionsPerLine;
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            if (currentSelection + optionsPerLine < options.Count)
+                                currentSelection += optionsPerLine;
+                            break;
+                        }
+                    case ConsoleKey.Escape:
+                        {
+                            if (canCancel)
+                                return -1;
+                            break;
+                        }
+                }
+            } while (key != ConsoleKey.Enter);
+
+            Console.CursorVisible = true;
+
+            return currentSelection;
+        }
     }
 }
+
+
